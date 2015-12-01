04c89816e25c138af0b581380ecd2be4f6549c60
Mike Fikes <mike@fikesfarm.com>
2015-11-30 21:51:01 -0500

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ignore-pods
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/ignore_pods/core.cljs
@@ -0,0 +1,45 @@
+(ns ignore-pods.core
+  (:require [om.core :as om])
+  (:require-macros [natal-shell.components :refer [view text image touchable-highlight]]
+                   [natal-shell.alert-ios :refer [alert]]))
+
+(set! js/React (js/require "react-native/Libraries/react-native/react-native.js"))
+
+(defonce app-state (atom {:text "Welcome to IgnorePods"}))
+
+(defn widget [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (try
+        (view {:style {:flexDirection "column" :margin 40 :alignItems "center"}}
+          (text
+            {:style {:fontSize 50 :fontWeight "100" :marginBottom 20 :textAlign "center"}}
+            (:text data))
+
+          (image {:source {:uri "https://raw.githubusercontent.com/cljsinfo/logo.cljs/master/cljs.png"}
+                  :style {:width 80 :height 80 :marginBottom 30}})
+
+          (touchable-highlight
+            {:style {:backgroundColor "#999" :padding 10 :borderRadius 5}
+             :onPress #(alert "HELLO!")}
+            (text {:style {:color "white" :textAlign "center" :fontWeight "bold"}} "press me")))
+
+        (catch :default e
+          (view {:style {:backgroundColor "#cc0814" :flex 1 :padding 20 :paddingTop 40}}
+            (text
+              {:style {:fontWeight "normal" :color "white" :fontSize 24 :marginBottom 10}}
+              "ERROR")
+            (text
+              {:style {:color "white" :fontFamily "Menlo-Regular" :fontSize 16 :lineHeight 24}}
+              (.-message e))
+            (text
+              {:style {:color "white" :marginTop 20 :fontSize 16 :fontWeight "bold"}}
+              "Check REPL log for details.")))))))
+
+
+(om/root widget app-state {:target 1})
+
+(defn ^:export init []
+  ((fn render []
+     (.requestAnimationFrame js/window render))))
